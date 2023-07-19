eb02afdb51e13ce112a4fad47628587adebfc81d
manuel-sperrle-stb-de <manuel.sperrle@stb.de>
2023-07-19 19:25:50 +0200

    reworked: Get-HardwareResults.ps1

--- Get-HardwareResults.ps1
+++ Get-HardwareResults.ps1
@@ -1,6 +1,14 @@
+#Requires -Modules @{ ModuleName='Microsoft.Graph.Intune'; RequiredVersion='6.1907.1.0'; }
+
 param (
     $DeviceManagementScriptId = (Get-Content .\DeviceManagementScriptId.env )
 )
 
-Connect-MSGraph
-(Invoke-MSGraphRequest -HttpMethod GET -Url ('deviceManagement/deviceManagementScripts/{0}/deviceRunStates?$expand=managedDevice' -f $DeviceManagementScriptId) | Get-MSGraphAllPages | Where-Object -Property errorCode -EQ 0).resultMessage | ConvertFrom-Json
+Update-MSGraphEnvironment -SchemaVersion 'beta' | Write-Host
+Connect-MSGraph | Write-Host
+
+Invoke-MSGraphRequest -HttpMethod GET -Url ('deviceManagement/deviceManagementScripts/{0}/deviceRunStates?$expand=managedDevice' -f $DeviceManagementScriptId ) `
+| Get-MSGraphAllPages `
+| Where-Object -Property errorCode -EQ 0 `
+| Select-Object -ExpandProperty resultMessage `
+| ConvertFrom-Json
