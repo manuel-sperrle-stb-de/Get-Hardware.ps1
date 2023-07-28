04511c49f36f27e56516b12d36a42d9686f62f27
manuel-sperrle-stb-de <manuel.sperrle@stb.de>
2023-07-28 06:48:50 +0200

    added workspace / changed logic

--- Get-Hardware.ps1
+++ Get-Hardware.ps1
@@ -1,86 +1,24 @@
 $CimInstance = @{}
 @('ComputerSystem','Bios','Processor','PhysicalMemory','LogicalDisk','VideoController','NetworkAdapterConfiguration') | ForEach-Object {
-    ($CimInstance).$_ = Get-CimInstance ('Win32_{0}' -f $_)
+    ($CimInstance).$_ = Get-CimInstance ('Win32_{0}' -f $_) | Select-Object -Property * -ExcludeProperty Cim* | ConvertTo-Json -WarningAction SilentlyContinue -InformationAction SilentlyContinue | ConvertFrom-Json
 }
 
 [PSCustomObject]@{
 
+    # flat
     TimeStamp = Get-Date -Format s
-
     Name = $CimInstance.ComputerSystem.Name
     SerialNumber = $CimInstance.Bios.SerialNumber
 
-    ComputerSystem = $CimInstance.ComputerSystem | Select-Object @(
-        'Name'
-        'Domain'
-        'Manufacturer'
-        'Model'
-        'OEMStringArray'
-        'PrimaryOwnerName'
-    )
-
-    Bios = $CimInstance.Bios | Select-Object @(
-        'Name'
-        'Manufacturer'
-        'Version'
-        'SerialNumber'
-        'SMBIOSBIOSVersion'
-    ) 
-
-    Processor = $CimInstance.Processor | Select-Object @(
-        'Name'
-        'DeviceID'
-        'Manufacturer'
-        'SocketDesignation'
-        'NumberOfCores'
-        'NumberOfLogicalProcessors'
-        'MaxClockSpeed'
-        'L2CacheSize'
-        'L3CacheSize'
-    )
+    # 1 object
+    ComputerSystem = $CimInstance.ComputerSystem
+    Bios = $CimInstance.Bios
+    Processor = $CimInstance.Processor
     
-    PhysicalMemory = $CimInstance.PhysicalMemory | Select-Object @(
-        'PartNumber'
-        'Manufacturer'
-        'BankLabel'
-        'Capacity'
-        'Speed'
-        'FormFactor'
-    )
-
-    LogicalDisk = $CimInstance.LogicalDisk | Select-Object @(
-        'DeviceID'
-        'DriveType'
-        'MediaType'
-        'VolumeName'
-        'VolumeSerialNumber'
-        'Size'
-        'FreeSpace'
-        'FileSystem'
-        'MaximumComponentLength'
-    )
-
-    VideoController = $CimInstance.Videocontroller | Select-Object @(
-        'Name' 
-        'PNPDeviceID'
-        'VideoProcessor'
-        'AdapterRAM'
-        'InstalledDisplayDrivers'
-        'DriverDate'
-        'DriverVersion'
-    )
-
-    NetworkAdapterConfiguration = $CimInstance.NetworkAdapterConfiguration | Select-Object @(
-        'Index'
-        'ServiceName'
-        'DHCPEnabled'
-        'Description'
-        'MACAddress'
-        'IPEnabled'
-        'DHCPServer'
-        'DHCPDomain'
-        'IPAddress'
-        'DefaultIPGateway'
-    )
+    # array of objects
+    PhysicalMemory = @($CimInstance.PhysicalMemory)
+    LogicalDisk = @($CimInstance.LogicalDisk)
+    VideoController = @($CimInstance.Videocontroller)
+    NetworkAdapterConfiguration = @($CimInstance.NetworkAdapterConfiguration)
 
 } | ConvertTo-Json -Depth 5 -Compress
