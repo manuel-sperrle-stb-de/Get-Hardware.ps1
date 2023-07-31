2345db119dbcd420e67735aceae6f27b52ae1cc8
manuel-sperrle-stb-de <manuel.sperrle@stb.de>
2023-07-31 10:03:09 +0200

    split: function Get-HardwareResults add: Get-HardwareResults script

---
+++ Get-HardwareResults.Function.ps1
@@ -0,0 +1,17 @@
+Function Get-HardwareResults {
+    #Requires -Modules @{ ModuleName='Microsoft.Graph.Intune'; RequiredVersion='6.1907.1.0'; }
+
+    param (
+        $DeviceManagementScriptId = (Get-Content (Join-Path $PSScriptRoot 'DeviceManagementScriptId.env') )
+    )
+    
+    Update-MSGraphEnvironment -SchemaVersion 'beta' | Write-Host
+    Connect-MSGraph | Write-Host
+    
+    Invoke-MSGraphRequest -HttpMethod GET -Url ('deviceManagement/deviceManagementScripts/{0}/deviceRunStates?$expand=managedDevice' -f $DeviceManagementScriptId ) `
+    | Get-MSGraphAllPages `
+    | Where-Object -Property errorCode -EQ 0 `
+    | Select-Object -ExpandProperty resultMessage `
+    | ConvertFrom-Json    
+
+}
--- Get-HardwareResults.ps1
+++ Get-HardwareResults.ps1
@@ -1,14 +1,5 @@
-#Requires -Modules @{ ModuleName='Microsoft.Graph.Intune'; RequiredVersion='6.1907.1.0'; }
-
-param (
-    $DeviceManagementScriptId = (Get-Content (Join-Path $PSScriptRoot 'DeviceManagementScriptId.env') )
-)
-
-Update-MSGraphEnvironment -SchemaVersion 'beta' | Write-Host
-Connect-MSGraph | Write-Host
-
-Invoke-MSGraphRequest -HttpMethod GET -Url ('deviceManagement/deviceManagementScripts/{0}/deviceRunStates?$expand=managedDevice' -f $DeviceManagementScriptId ) `
-| Get-MSGraphAllPages `
-| Where-Object -Property errorCode -EQ 0 `
-| Select-Object -ExpandProperty resultMessage `
-| ConvertFrom-Json
+Get-ChildItem $PSScriptRoot -File -Filter '*.Function.ps1' | ForEach-Object {
+    $_.BaseName.TrimEnd('.Function')
+    . $_.FullName
+}
+Get-HardwareResults | ConvertTo-Json -Depth 5 | Out-File '.\Get-Hardware.json'
